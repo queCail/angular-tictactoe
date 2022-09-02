e33d018cab16739e353d6c6a1c9b8c8507f3bf20
Caique L <caiqueluisdossantos@gmail.com>
2022-09-02 14:39:58 -0300

    better board and add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { StartButtonComponent } from './start-button/start-button.component';
 import { HomepageComponent } from './homepage/homepage.component';
@@ -16,7 +16,16 @@ import { BoardComponent } from './board/board.component';
     BoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'home', pathMatch: 'full'
+      }, {
+        path:'home', component: HomepageComponent
+      }, {
+        path: 'game', component: BoardComponent
+      }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
